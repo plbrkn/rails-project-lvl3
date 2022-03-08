1deabd136d81d8feef96f2165738c457784be533
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-03-08 19:26:48 +0800

    added test for bulletins

--- test/controllers/web/bulletins_controller_test.rb
+++ test/controllers/web/bulletins_controller_test.rb
@@ -3,7 +3,34 @@
 require 'test_helper'
 
 class Web::BulletinsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @attrs = {
+      name: Faker::Job.title,
+      body: Faker::Lorem.paragraph_by_chars(number: 150),
+      category_id: categories(:one).id,
+      photo: fixture_file_upload('cat.jpeg', 'image/jpeg')
+    }
+
+    sign_in(users(:one))
+  end
+
+  test 'should get index' do
+    get root_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_bulletin_path
+    assert_response :success
+  end
+
+  test 'should create bulletin' do
+    post bulletins_path, params: { bulletin: @attrs }
+
+    assert_response :redirect
+
+    new_bulletin = Bulletin.find_by(name: @attrs[:name])
+
+    # assert { new_bulletin }
+  end
 end
