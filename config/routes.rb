6269ade0e9f1f7ebc86e9cfefb1ecd4fd6fd202b
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-03-30 16:51:45 +0800

    added namespace admin

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,13 @@ Rails.application.routes.draw do
     resources :bulletins, except: :destroy
     resources :users, only: %i[new create]
 
+    namespace :admin do
+      root 'home#index'
+      resources :categories
+      resources :users, only: %i[index edit update]
+      resources :bulletins, except: :destroy
+    end
+
     post 'auth/:provider', to: 'auth#request', as: :auth_request
     get 'auth/:provider/callback', to: 'auth#callback', as: :callback_auth
   end
