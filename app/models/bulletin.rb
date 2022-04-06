42c2e70b6c42915e9cf5ba6981b556adafb7f174
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-04-06 21:14:40 +0800

    added state in Bulletin model

--- app/models/bulletin.rb
+++ app/models/bulletin.rb
@@ -1,6 +1,32 @@
 # frozen_string_literal: true
 
 class Bulletin < ApplicationRecord
+  include AASM
+
+  aasm column: 'state' do
+    state :draft, initial: true
+    state :under_moderation
+    state :published
+    state :rejected
+    state :archived
+
+    event :moderation do
+      transitions from: :draft, to: :under_moderation
+    end
+
+    event :publish do
+      transitions from: :under_moderation, to: :published
+    end
+
+    event :reject do
+      transitions from: :under_moderation, to: :draft
+    end
+
+    event :archive do
+      transitions from: %i[draft under_moderation published rejected], to: :archived
+    end
+  end
+
   belongs_to :category
   belongs_to :user
   has_one_attached :photo
