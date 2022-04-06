00641faaa76cbb9ea555475d5a627b5ef58dd016
Pavel Polubarkin <pavel.polubarkin@gmail.com>
2022-04-06 21:17:08 +0800

    added categories in admin namespace

---
+++ test/controllers/web/admin/categories_controller_test.rb
@@ -0,0 +1,46 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+class Web::Admin::CategoriesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @category = categories(:one)
+    @attrs = {
+      name: Faker::Job.title
+    }
+
+    sign_in(users(:one))
+  end
+
+  test 'should get index' do
+    get admin_categories_url
+    assert_response :success
+  end
+
+  test 'should get new' do
+    get new_admin_category_path
+    assert_response :success
+  end
+
+  test 'should create category' do
+    post admin_categories_path, params: { category: @attrs }
+
+    assert_response :redirect
+
+    assert Category.find_by(name: @attrs[:name])
+  end
+
+  test 'should get edit' do
+    get edit_admin_category_path @category
+    assert_response :success
+  end
+
+  test 'should update category' do
+    patch admin_category_path @category, params: { category: @attrs }
+
+    assert_response :redirect
+
+    @category.reload
+    assert @attrs[:name] == @category.name
+  end
+end
